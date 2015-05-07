1ef3b6e7e68f5ade0175897d5e0ced3efbc421f4
Sori Kang <sorikuya@gmail.com>
2015-05-07 14:13:09 +1000

    remove public folder because it is unncessary hierarchy

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.use(function(req, res, next) {
     next();
 });
 
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname));
 app.use(express.static('bower_components'));
 app.use(express.static(__dirname + '/views'));
 
